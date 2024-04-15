3325112af3ce6abcce493d90090d2c78a4797f85
ChTole <ch.ariel.tole@gmail.com>
2024-04-15 20:54:38 -0300

    Agrego clases de Git y clase 13 (intro JS)

---
+++ Clase13/js/intro.js
@@ -0,0 +1,29 @@
+// Comentario en línea
+
+/*
+Comentario 
+de más
+de una
+línea
+*/
+
+// 1-Tipos de datos
+
+// Primitivos
+// Boolean, Number, BigInt, String, Symbol y undefined
+
+// Complejos
+// Array - arreglo (vector) -> Python == lista
+// Object - objeto (atributos y métodos) -> Python == diccionario
+typeof "Cadena de texto"
+typeof "123"
+typeof 123
+typeof 3.14 
+typeof true
+
+// 2-Debilmente tipado
+
+typeof "123" + 1 // "1231"
+typeof "123" - 1 // 122
+
+console.log("Intro a JS - Desde un script externo")
