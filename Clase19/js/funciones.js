7786b27ced859c089de99415a43369d25e0a23fb
ChTole <ch.ariel.tole@gmail.com>
2024-05-08 17:21:56 -0300

    Agrego plantillas clase 19

---
+++ Clase19/js/funciones.js
@@ -0,0 +1,13 @@
+// Funciones
+
+// Fundamento y tiempos de una función
+
+// Parámetros y retorno - ámbito de los parámetros
+
+// Función como expresión
+
+// Función anónima - IIFE (Expresión de función inmediatamente invocada)
+
+// Callback
+
+// Función flecha
---
+++ Clase19/js/validar-form.js
@@ -0,0 +1,7 @@
+// Validación de formulario
+console.log('Comienzo');
+
+
+
+console.log('Fin del algoritmo (?)');
+// Aclaración: los "testigos" al principio y al fin son para ayudar a comprender el comportamiento asincrónico del script.
